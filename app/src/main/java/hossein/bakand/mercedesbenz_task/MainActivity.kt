95a974715409f0174113786294579ada89ecace0
Hossein Bakand <h.bakand@yahoo.com>
2023-06-06 23:01:50 +0430

    initial commit

---
+++ app/src/main/java/hossein/bakand/mercedesbenz_task/MainActivity.kt
@@ -0,0 +1,43 @@
+package hossein.bakand.mercedesbenz_task
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import hossein.bakand.mercedesbenz_task.ui.theme.MercedesBenzTaskTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MercedesBenzTaskTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MercedesBenzTaskTheme {
+        Greeting("Android")
+    }
+}
