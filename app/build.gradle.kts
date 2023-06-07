f03f5978c6de0cfaf72d16ae6d9aa6b33835e3e9
Hossein Bakand <h.bakand@yahoo.com>
2023-06-07 23:21:04 +0430

    - create buildSrc Module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,26 +52,21 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.5.1")
-    implementation(platform("androidx.compose:compose-bom:2022.10.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.core:core-ktx:+")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2022.10.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(Libraries.CoreKtx)
+    implementation(Libraries.ComposeActivity)
+    implementation(Libraries.ComposeUi)
+    implementation(Libraries.ComposeUiGraphic)
+    implementation(Libraries.ComposeTooling)
+    implementation(Libraries.ComposeToolingPreview)
+    implementation(Libraries.ComposeMaterial)
+    implementation(platform(Libraries.KotlinBom))
+    implementation(platform(Libraries.ComposeBom))
+    implementation(Libraries.Lifecycle)
 
-    implementation("com.google.dagger:hilt-android:2.46.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.46.1")
+    testImplementation(TestLibraries.Junit)
+
+    implementation(Libraries.Hilt)
+    kapt(Libraries.HiltCompiler)
 }
 
 // Allow references to generated code
